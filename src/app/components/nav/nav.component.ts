d6e963fcdbf2a60b3e0478e60d6932429c510356
vamsiparimi <vamsiparimi99@gmail.com>
2024-09-01 13:39:22 +0530

    added quantity badge for cart

--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, OnDestroy, ChangeDetectorRef } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { AuthService } from '../../services/auth.service';
+import { CartService } from '../../services/cart.service';
 import { Subscription } from 'rxjs';
 
 @Component({
@@ -15,9 +16,14 @@ export class NavComponent implements OnInit, OnDestroy {
   isAdmin: boolean = false;
   isLoggedIn: boolean = false;
   isMenuOpen = false;
+  cartItemCount : number = 0;
   private subscriptions: Subscription = new Subscription();
 
-  constructor(public authService: AuthService, private cdr: ChangeDetectorRef) {}
+  constructor(
+    public authService: AuthService,
+    private cartService: CartService, 
+    private cdr: ChangeDetectorRef
+  ) {}
 
   ngOnInit(): void {
     this.subscriptions.add(
@@ -29,6 +35,12 @@ export class NavComponent implements OnInit, OnDestroy {
     this.subscriptions.add(
       this.authService.isAdmin$.subscribe(status => {
         this.isAdmin = status;
+        this.cdr.markForCheck(); // Mark for check to update the view
+      })
+    );
+    this.subscriptions.add(
+      this.cartService.getCartItems().subscribe(items => {
+        this.cartItemCount = items.length; // Update cart item count
         this.cdr.markForCheck(); // Mark for check to update the view
       })
     );
